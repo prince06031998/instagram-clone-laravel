5ac17c73b50c0751fe7437e2f3d7ddbde817351c
prince06031998 <kuti.12598@gmail.com>
2022-03-10 11:40:39 +0700

    postModel

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,8 @@ class User extends Authenticatable
     public function getAuthPassword() {
         return $this->UserEmail;
     }
+
+    public function posts(){
+        return $this->hasMany(Post::class,'userId');
+    }
 }
---
+++ database/migrations/2022_03_10_042035_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('status');
+            $table->json('images');
+            $table->timestamps();
+        });
+        Schema::table('posts', function (Blueprint $table) {
+            $table->unsignedBigInteger('userId');
+            $table->foreign('userId')->references('id')->on('users')->onDelete('cascade');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
